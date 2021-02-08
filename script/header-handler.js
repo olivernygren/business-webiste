1b833b129a45391b4195fdaef13ac391e12cc9f9
Oliver Nygren <oliver.nygren@icloud.com>
2021-02-08 21:20:27 +0100

    added header scrollY bg-color trigger

---
+++ script/header-handler.js
@@ -0,0 +1,11 @@
+const header  = document.getElementById('header')
+
+window.onscroll = () => {
+  if (scrollY > 100) {
+    header.style.background = 'white';
+    header.style.boxShadow = '0px 4px 8px rgba(0, 0, 0, 0.2)';
+  } else {
+    header.style.background = 'transparent';
+    header.style.boxShadow = 'none';
+  }
+}
